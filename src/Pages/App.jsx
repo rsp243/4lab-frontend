48614a0d4c6b0804b78d7edcd5d24839d56235d7
Reshetov Semjon <reshetov.semjon@gmail.com>
2023-12-17 19:30:43 +0300

    feat: add handleAnotherAttempt function

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -85,6 +85,40 @@ export default function App({ getToken }) {
 			});
 	}
 
+	const handleAnotherAttemptClick = async e => {
+		e.preventDefault();
+
+		let data = {
+			token: getToken()
+		}
+
+		axios.post(`http://localhost:8080/api/v1/point/delete`, data)
+			.then(res => {
+				console.log(res.status);
+				console.log(res.data);
+				setResults([])
+				msgs.current.show([
+					{ sticky: false, life: 2000, severity: 'success', summary: 'Success', detail: res.data.message, closable: false },
+				])
+			})
+			.catch(function (error) {
+				let myError = "";
+				if (error.response) {
+					// The request was made and the server responded with a status code
+					// that falls out of the range of 2xx
+					console.log(error.response.data);
+					myError = error.response.data.status + " " + error.response.data.message
+				} else {
+					// Something happened in setting up the request that triggered an Error
+					console.log('Error', error.message);
+					myError = "An error during request setting up has happened"
+				}
+				msgs.current.show([
+					{ severity: 'error', life: 5000, summary: 'Error', detail: myError, sticky: false, closable: false }
+				]);
+			});
+	}
+
 	return (
 		<div className="App">
 			<div className="card flex flex-column justify-content-center align-items-center">
@@ -94,7 +128,9 @@ export default function App({ getToken }) {
 					yValue={yValue} setYValue={setYValue}
 					rValue={rValue} setRValue={setRValue}
 				/>
-				<ButtonBlock handleThrowClick={handleThrowClick} />
+				<ButtonBlock 
+					handleThrowClick={handleThrowClick} 
+					handleAnotherAttemptClick={handleAnotherAttemptClick} />
 				<Messages ref={msgs} />
 				<ResultTable results={results} />
 			</div>
--- src/components/App/ActionButtonBlock.jsx
+++ src/components/App/ActionButtonBlock.jsx
@@ -2,15 +2,16 @@ import PropTypes from 'prop-types';
 
 import { Button } from 'primereact/button';
 
-export default function ButtonBlock({handleThrowClick}) {
+export default function ButtonBlock({handleThrowClick, handleAnotherAttemptClick}) {
     return (
         <div className="card mb-6">
             <Button className="mx-3" label="Throw An Axe" icon="pi pi-check" iconPos="right" onClick={handleThrowClick} />
-            <Button className="mx-3" label="Start Another Attempt" icon="pi pi-refresh" iconPos="right" />
+            <Button className="mx-3" label="Start Another Attempt" icon="pi pi-refresh" iconPos="right" onClick={handleAnotherAttemptClick} />
         </div>
     );
 }
 
 ButtonBlock.propTypes = {
     handleThrowClick: PropTypes.func.isRequired,
+    handleAnotherAttemptClick: PropTypes.func.isRequired,
 };
